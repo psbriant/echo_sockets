7ac158fd06cc2797fdd63c32570949af2f7b9c0f
pbriant <psbriant@q.com>
2017-01-16 12:35:10 -0800

    Initialize client socket and connect it to server.

--- echo_client.py
+++ echo_client.py
@@ -6,10 +6,14 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
+    sock = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    print('connecting to'
+          '{0} port {1}'.format(*server_address), file=log_buffer)
     # TODO: connect your socket to the server here.
-
+    sock.connect(server_address)
     # you can use this variable to accumulate the entire message received back
     # from the server
     received_message = ''
